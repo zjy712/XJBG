aea842eb5e161ffe139e10f973ba16486c2a597f
Jay Zhu <jayzhu@capvision.com>
2019-07-03 18:00:05 +0800

    test

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <One></One>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import One from '@/views/one.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    One
+  }
+}
+</script>
---
+++ src/views/one.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <h1>{{count}}</h1>
+    <button @click="add">增</button>  
+</div>
+    
+</template>
+
+<script>
+import {mapState,mapActions} from 'vuex'
+export default {
+    data() {
+        return {
+            // count:null
+        }
+    },
+    computed:{
+        ...mapState({
+            count: state => state.count
+        })
+    },
+    methods: {
+        ...mapActions({
+            add: 'increment'
+        })
+    },
+}
+</script>
+
